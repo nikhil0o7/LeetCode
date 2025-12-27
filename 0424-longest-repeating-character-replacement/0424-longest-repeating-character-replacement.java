f702178152fd2e519bed6ec09f6af84af105388b
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2025-12-26 23:05:14 -0500

    Time: 29 ms (22.72%), Space: 47.4 MB (7.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -5,7 +5,6 @@ class Solution {
         for(int r = 0; r < s.length(); r++){
             count.put(s.charAt(r), count.getOrDefault(s.charAt(r), 0) + 1);
             maxf = Math.max(maxf, count.get(s.charAt(r)));
-
             while((r - l + 1) - maxf > k){
                 count.put(s.charAt(l), count.getOrDefault(s.charAt(l), 0) - 1);
                 l++;
