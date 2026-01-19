53b55fb239ad5999e862618dfaee3e19560b042b
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-01-19 07:10:44 -0800

    Time: 15 ms (50.73%), Space: 47.6 MB (72.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -9,9 +9,8 @@ class Solution {
             arr.add(new int[] { entry.getKey(), entry.getValue() });
         }
         arr.sort((a,b) -> b[1] - a[1]);
-
         int[] res = new int[k];
-        for(int i=0; i < k; i++){
+        for(int i = 0; i < k; i++){
             res[i] = arr.get(i)[0];
         }
         return res;
