0f9500e9f5b11af697678e0dbd4f0fc6d17dd748
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-01-25 15:11:28 -0800

    Time: 7 ms (73.31%), Space: 22.4 MB (34.12%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,12 +6,10 @@ class Solution:
         merged = []
 
         for interval in intervals:
-
             if not merged or merged[-1][1] < interval[0]:
                 merged.append(interval)
             else:
                 merged[-1][1] = max(merged[-1][1], interval[1])
-
 
         return merged
 
