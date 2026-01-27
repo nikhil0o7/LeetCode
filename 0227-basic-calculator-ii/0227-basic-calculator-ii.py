6e8addf2bb52fea166035779e40cf5687c8bfb20
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-01-27 07:30:26 -0800

    Time: 39 ms (95.04%), Space: 22.8 MB (13.95%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        if not s: 
+            return 0
+
+        stack :List[int] = [] 
+        current = 0
+        op = '+'
+        s += '+'
+        for ch in s:
+            if ch.isdigit():
+                current = current * 10 + int(ch)
+            elif ch == ' ':
+                pass
+            else:
+                if op == '+':
+                    stack.append(current)
+                elif op == '-':
+                    stack.append(-current)
+                elif op == '*':
+                    stack.append(stack.pop() * current)
+                elif op =='/':
+                    prev = stack.pop()
+                    stack.append(math.trunc(prev/current))
+                op = ch
+                current = 0 
+        return sum(stack)
