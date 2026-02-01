a834a41cab90b3544162f2e5296e6a4357ee7f72
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-02-01 12:13:46 -0800

    Time: 918 ms (17.64%), Space: 24.3 MB (5.13%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,18 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res, dups = set(), set()
+        res , dups = set(), set()
         seen = {}
-        
         for i in range(len(nums) - 1):
             if nums[i] not in dups:
                 dups.add(nums[i])
-                
-                for j in range(i + 1, len(nums)):
-                    complement = -nums[i] - nums[j]
-                    
-                    if complement in seen and seen[complement] == i:
-                        res.add(tuple(sorted((nums[i], nums[j], complement))))
+            
+                for j in range(i+1, len(nums)):
+                    compliment = -nums[i] - nums[j]
+
+                    if compliment in seen and seen[compliment] == i:
+                        res.add(tuple(sorted((nums[i],nums[j],compliment))))
                     
                     seen[nums[j]] = i
-        
+
+
         return [list(x) for x in res]
