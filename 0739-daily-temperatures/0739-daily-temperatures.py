00394ffa4603c99769f7d2fce5d967190810bd48
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-03-04 19:19:15 -0800

    Time: 123 ms (23.09%), Space: 36.1 MB (7.67%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -5,6 +5,7 @@ class Solution:
         # save pair: [temp, index] - we can use index later to update the res
 
         for i, t in enumerate(temperatures):
+        #if we find the current temp is greater than the top element than we pop the top of the stack and set the results variable basically that's what the problem is to find the next top number
             while stack and t > stack[-1][0]:
                 stackT, stackI = stack.pop()
                 res[stackI] = i - stackI
