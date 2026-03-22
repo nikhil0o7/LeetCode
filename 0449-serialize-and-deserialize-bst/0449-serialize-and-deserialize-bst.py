f967b978f689fb08f79ed060c1116ada0aac8aaa
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-03-22 11:25:36 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
@@ -9,45 +9,42 @@ class Codec:
 
     def serialize(self, root):
         res = []
-
-        def preorder(node):
+        def preorder(node) -> None:
             if not node:
                 return
+
             res.append(str(node.val))
             preorder(node.left)
             preorder(node.right)
 
         preorder(root)
-        return ",".join(res)
+        print(res)
+        return "".join(res)
 
-        
+
 
     def deserialize(self, data):
         if not data:
-            return None
+            return []
 
-        vals = list(map(int, data.split(",")))
+        vals = [int(x) for x in data.split(",")]
         self.i = 0
-
-        def build(lower, upper):
+        def build(lower,upper) -> Optional[TreeNode]:
             if self.i == len(vals):
-                return None
+                return  None
 
             val = vals[self.i]
-
-            # BST constraint
             if val < lower or val > upper:
                 return None
-
             self.i += 1
-            node = TreeNode(val)
-
-            node.left = build(lower, val)
-            node.right = build(val, upper)
-
+            node = TreeNode(val)    
+           
+            left = build(lower,val)
+            right = build(val, lower)
+            node.left = left
+            node.right = right
             return node
-
-        return build(float("-inf"), float("inf"))
+        return build(float("-inf"),float("inf"))
 
 # Your Codec object will be instantiated and called as such:
 # Your Codec object will be instantiated and called as such:
