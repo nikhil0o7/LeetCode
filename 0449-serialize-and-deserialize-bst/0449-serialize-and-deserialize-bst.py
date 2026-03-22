d8de92470fc925c085291e3bcc35554d8cbfa66e
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-03-22 11:31:18 -0700

    Time: 77 ms (12.58%), Space: 22.6 MB (61.65%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.py
@@ -1,50 +1,59 @@
-# Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self,val: int = 0,left: Optional[TreeNode],right: Optional[TreeNode]):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Codec:
 
-    def serialize(self, root):
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        """Encodes a tree to a single string.
+        """
         res = []
         def preorder(node) -> None:
             if not node:
                 return
-
             res.append(str(node.val))
             preorder(node.left)
             preorder(node.right)
-
+        
         preorder(root)
         print(res)
         return ",".join(res)
+        
 
-
-
-    def deserialize(self, data):
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        """Decodes your encoded data to tree.
+        """
         if not data:
             return []
 
-        vals = [int(x) for x in data.split(",")]
-        self.i = 0
-        def build(lower,upper) -> Optional[TreeNode]:
-            if self.i == len(vals):
-                return  None
+        vals = []
 
+        for x in data.split(","):
+            vals.append(int(x))
+
+        print(vals)
+        self.i = 0
+        def build(lower,upper) -> Optiona[TreeNode]:
+            if self.i == len(vals):
+                return None
             val = vals[self.i]
+
             if val < lower or val > upper:
                 return None
-            self.i += 1
-            node = TreeNode(val)    
-           
+            
+            self.i +=1
+            node = TreeNode(val)
             left = build(lower,val)
             right = build(val, upper)
             node.left = left
             node.right = right
+
             return node
+
         return build(float("-inf"),float("inf"))
+        
 
 # Your Codec object will be instantiated and called as such:
 # Your Codec object will be instantiated and called as such:
