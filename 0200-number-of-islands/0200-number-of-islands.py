b1c34d3ce0e7475e57c189441b36168b37617a9e
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-03-22 17:45:15 -0700

    Time: 254 ms (38.08%), Space: 31.3 MB (8.78%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        m = len(grid)
-        n = len(grid[0])
-        seen = set()
+        ROWS = len(grid)
+        COLS = len(grid[0])
         ans = 0
-        directions = [(0,1),(1,0),(0, -1), (-1,0)]
-        def valid_cell(row, col):
-            return 0 <= row < m and 0 <= col < n and grid[row][col] == "1"
+        seen = set()
+        directions = [(1,0),(-1,0),(0,-1),(0,1)]
 
-        def dfs(row,col):
+        def valid_cell(r,c):
+            return 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == "1"
+
+        def dfs(r,c):
             for dx,dy in directions:
-                next_row , next_col = row + dx, col + dy
-                if valid_cell(next_row,next_col) and (next_row,next_col) not in seen:
-                    seen.add((next_row,next_col))
-                    dfs(next_row, next_col)
+                next_dx, next_dy = r + dx, c + dy
+                if valid_cell(next_dx, next_dy) and (next_dx,next_dy) not in seen:
+                    seen.add((next_dx,next_dy))
+                    dfs(next_dx,next_dy)
 
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == "1" and (row, col) not in seen:
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col] == "1" and (row,col) not in seen:
                     ans += 1
                     seen.add((row,col))
                     dfs(row,col)
 
         return ans
-
-
-
-        
