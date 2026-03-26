608b6fe0a6c214e3f159a307cfa19c9526875496
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-03-26 06:18:01 -0700

    Time: 3 ms (30.59%), Space: 21.2 MB (94.92%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,12 +1,11 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-
         if not intervals:
             return [newInterval]
-
+        
         n = len(intervals)
         target = newInterval[0]
-        left,right = 0, n-1
+        left, right = 0, n - 1
 
         while left <= right:
             mid = (left + right) // 2
@@ -16,13 +15,15 @@ class Solution:
                 right = mid - 1
 
         intervals.insert(left, newInterval)
-        res = []
-        for interval in intervals:
 
+        res = []
+
+        for interval in intervals:
             if not res or res[-1][1] < interval[0]:
                 res.append(interval)
-
             else:
                 res[-1][1] = max(res[-1][1], interval[1])
 
         return res
+
+        
