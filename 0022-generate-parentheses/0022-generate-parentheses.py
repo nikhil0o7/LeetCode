df80212a2231279282a18702dd1940a90a188d9f
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-03-30 17:43:32 -0700

    Time: 0 ms (100%), Space: 19.5 MB (23.54%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,11 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        def backtrack(curr, openN, closedN):
-            if len(curr) == n * 2:
-                res.append("".join(curr))
+        def backtrack(curr, openN, closedN) -> None:
+            if openN == closedN == n:
+                res.append("".join((curr[:])))
                 return
-            
+
+            if openN > n or openN < closedN:
+                return
+
             if openN < n:
                 curr.append("(")
                 backtrack(curr, openN + 1, closedN)
@@ -16,6 +19,6 @@ class Solution:
                 backtrack(curr, openN, closedN + 1)
                 curr.pop()
 
-        backtrack([],0,0)
+        backtrack([], 0, 0)
         return res
         
