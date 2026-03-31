7e64b697571f21ba0e587b8981f7934ba15b2040
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-03-30 21:07:59 -0700

    Time: 4 ms (24.28%), Space: 20.4 MB (79.6%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,24 +1,25 @@
 class DSU:
     def __init__(self,n):
         self.comps = n
-        self.Parent = list(range(n + 1))
-        self.Size = [1] * (n + 1)
+        self.Parent = list(range(n))
+        self.Size = [1] * (n) 
 
-    def find(self, node):
+    def find(self,node):
         if self.Parent[node] != node:
             self.Parent[node] = self.find(self.Parent[node])
         return self.Parent[node]
 
-    def union(self,u,v) -> bool:
+    def union(self,u,v):
         pu = self.find(u)
         pv = self.find(v)
         if pu == pv:
             return False
-        self.comps -=1
-        if self.Size[pu] < self.Size[pv]:
-            pu,pv = pv,pu
-        self.Size[pu] += self.Size[pv]
+        self.comps -= 1
+        if self.Size[pv] < self.Size[pu]:
+            pv,pu = pu,pv
+        self.Size[pu] += self.Size[pu]
         self.Parent[pv] = pu
+
         return True
 
     def components(self):
@@ -28,12 +29,16 @@ class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         if len(edges) > n - 1:
             return False
-
+        
         dsu = DSU(n)
 
         for u,v in edges:
-            print(u,v)
-            if not dsu.union(u, v):
+            if not dsu.union(u,v):
                 return False
 
-        return dsu.components() == 1        
+        return dsu.components() == 1
+
+        
+
+
+        
