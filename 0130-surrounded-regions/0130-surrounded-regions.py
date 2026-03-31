70e767206bdf0277c06f1701a65c515a26fde9e2
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-03-30 20:57:00 -0700

    Time: 3 ms (85.35%), Space: 22.4 MB (67.88%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,21 +5,22 @@ class Solution:
         """
         ROWS,COLS = len(board), len(board[0])
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
-
         q = deque()
+
         for r in range(ROWS):
             for c in range(COLS):
-                if(r == 0 or r == ROWS - 1 or c == 0 or c == COLS-1) and board[r][c] == "O":
+                if(r == 0 or r == ROWS - 1 or c == 0 or c == COLS - 1) and board[r][c] == "O":
                     q.append((r,c))
 
+        
         while q:
             r,c = q.popleft()
             if board[r][c] == "O":
                 board[r][c] = "T"
                 for dx,dy in directions:
-                    nr,nc = r + dx, c + dy
-                    if 0 <= nr < ROWS and 0 <= nc < COLS:
-                        q.append((nr,nc))
+                    curr_r , curr_c = dx + r, dy + c
+                    if (0 <= curr_r < ROWS and 0 <= curr_c < COLS):
+                        q.append((curr_r,curr_c))
 
         for r in range(ROWS):
             for c in range(COLS):
@@ -28,5 +29,3 @@ class Solution:
                 if board[r][c] == "T":
                     board[r][c] = "O"
 
-
-            
