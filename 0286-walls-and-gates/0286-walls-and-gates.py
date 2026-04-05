36c70855ae0f62072d5b635c5ff2639d0c8cf84f
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-05 14:09:46 -0700

    Time: 44 ms (42.31%), Space: 25.7 MB (28.48%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -5,9 +5,9 @@ class Solution:
         """
         ROWS = len(rooms)
         COLS = len(rooms[0])
-        queue = deque()
-        seen = set()
         dist = 0
+        seen = set()
+        queue = deque()
 
         for r in range(ROWS):
             for c in range(COLS):
@@ -16,7 +16,7 @@ class Solution:
                     seen.add((r,c))
 
         def addRoom(r,c):
-            if (0 > r or r == ROWS or 0 > c or c == COLS or (r,c) in seen or rooms[r][c] == -1):
+            if (0 > r or r == ROWS or 0 > c or c == COLS or rooms[r][c] == -1 or (r,c) in seen):
                 return
             queue.append((r,c))
             seen.add((r,c))
@@ -30,4 +30,7 @@ class Solution:
                 addRoom(r - 1, c)
                 addRoom(r, c + 1)
                 addRoom(r, c - 1)
+
             dist += 1
+
+        
