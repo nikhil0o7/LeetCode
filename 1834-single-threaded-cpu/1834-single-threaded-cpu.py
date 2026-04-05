15b38bea2552f451e68ae2c069f3546143f34113
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-05 15:19:11 -0700

    Time: 276 ms (90.28%), Space: 61.8 MB (88.74%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,24 +1,27 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        tasks = sorted([(start,dur,i) for i,(start,dur) in enumerate(tasks)],reverse=True)
+        tasks = [(start,dur,i) for i, (start,dur) in enumerate(tasks)]
+        tasks.sort(reverse = True)
         next_available_t = 0
         queued_tasks = []
-        
-        res = []
-        # start, dur, id
-        while queued_tasks or tasks:
-            # step 1
-            # start or gap between tasks
+
+        ans = []
+
+        while tasks or queued_tasks:
+
             if not queued_tasks and next_available_t <= tasks[-1][0]:
                 next_available_t = tasks[-1][0]
-        
-            # queue 'expired tasks'
+
+
             while tasks and tasks[-1][0] <= next_available_t:
                 start,dur,idx = tasks.pop()
-                heappush(queued_tasks, (dur, idx))
-            # step 2
-            dur,idx = heappop(queued_tasks)
+                heapq.heappush(queued_tasks, (dur,idx))
+
+            dur,idx = heapq.heappop(queued_tasks)
             next_available_t += dur
-            res.append(idx)
+            ans.append(idx)
+
+        return ans
+
+
         
-        return res
