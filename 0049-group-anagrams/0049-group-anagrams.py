ef98942589fcb46c207228bfc572e82aa08bfbec
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-06 20:46:12 -0700

    Time: 9 ms (90.37%), Space: 21.8 MB (89.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = {}
+        d = defaultdict(list)
         for s in strs:
-            sorted_s = "".join(sorted(s))
-            if sorted_s in d:
-                d[sorted_s].append(s)
+            sorted_str = "".join(sorted(s))
+            if sorted_str in d:
+                d[sorted_str].append(s)
             else:
-                d[sorted_s] = [s]
-        return list(d.values())
+                d[sorted_str] = [s]
 
+        return list(d.values())
         
