e2c7f249c76374aa1618b18ae5d38874b49d7699
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-06 21:21:34 -0700

    Time: 63 ms (93.99%), Space: 19.6 MB (46.34%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,29 +1,26 @@
 class Codec:
-    def encode(self, strs: List[str]) -> str:
-        """Encodes a list of strings to a single string.
-        """
+    def encode(self, strs):
+        # Initialize an empty string to hold the encoded string.
         encoded_string = ''
         for s in strs:
-            encoded_string += str(len(s)) + ':/' + s
+            # Append the length, the delimiter, and the string itself.
+            encoded_string += str(len(s)) + '/:' + s
         print(encoded_string)
         return encoded_string
 
-    def decode(self, s: str) -> List[str]:
-        """Decodes a single string to a list of strings.
-        """
-        decoded_string = []
+    def decode(self, s):
+        # Initialize a list to hold the decoded strings.
+        decoded_strings = []
         i = 0
         while i < len(s):
-            delim = s.find(":/", i)
-            len_str = int(s[i : delim])
-            str_ = s[delim + 2 : delim + 2 + len_str]
-            decoded_string.append(str_)
-            i = delim + 2 + len_str
-
-        return decoded_string
-        
-
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+            # Find the delimiter.
+            delim = s.find('/:', i)
+            # Get the length, which is before the delimiter.
+            length = int(s[i:delim])
+            # Get the string, which is of 'length' length after the delimiter.
+            str_ = s[delim+2 : delim+2+length]
+            # Add the string to the list.
+            decoded_strings.append(str_)
+            # Move the index to the start of the next length.
+            i = delim + 2 + length
+        return decoded_strings
