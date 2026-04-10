7f90d18a53b93d85c65613f695abadb39e66b70b
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-09 20:03:52 -0700

    Time: 127 ms (17.2%), Space: 35.9 MB (8.52%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,13 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
-        stack = []
+        stack = [] # we need to build a monotonic stack
+        # save pair: [temp, index] - we can use index later to update the res
 
-        for i,t in enumerate(temperatures):
+        for i, t in enumerate(temperatures):
+        #if we find the current temp is greater than the top element than we pop the top of the stack and set the results variable basically that's what the problem is to find the next top number
             while stack and t > stack[-1][0]:
-                temp,index = stack.pop()
-                res[index] = i - index
-
+                stackT, stackI = stack.pop()
+                res[stackI] = i - stackI
             stack.append([t,i])
-
-        return res        
+        return res
