6a673ba33bdfbdcaf238d3ec385d6709b2c615bc
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-11 11:41:55 -0700

    Time: 302 ms (75.62%), Space: 62.8 MB (67.4%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,24 +1,27 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        tasks = sorted([(start,dur,i) for i,(start,dur) in enumerate(tasks)],reverse=True)
-        next_available_t = 0
+        tasks = [(start,dur, idx) for idx,(start,dur) in enumerate(tasks)]
+        tasks.sort(reverse = True)
         queued_tasks = []
-        
+        next_available_t = 0
+
         res = []
-        # start, dur, id
+
         while queued_tasks or tasks:
-            # step 1
-            # start or gap between tasks
+
             if not queued_tasks and next_available_t <= tasks[-1][0]:
                 next_available_t = tasks[-1][0]
-        
-            # queue 'expired tasks'
+
+
             while tasks and tasks[-1][0] <= next_available_t:
                 start,dur,idx = tasks.pop()
-                heappush(queued_tasks, (dur, idx))
-            # step 2
+                heapq.heappush(queued_tasks, (dur,idx))
+
+            
             dur,idx = heappop(queued_tasks)
             next_available_t += dur
             res.append(idx)
-        
         return res
+
+
+        
