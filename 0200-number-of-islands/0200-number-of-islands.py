ea02329f46c66840d22d2c06639e8e5a4670ddbe
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-19 10:14:29 -0700

    Time: 277 ms (14.05%), Space: 31.5 MB (5.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,24 +3,28 @@ class Solution:
         ROWS = len(grid)
         COLS = len(grid[0])
         ans = 0
+        seen = set()
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
-        
+
         def valid_cell(r,c) -> bool:
             return 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == "1"
 
-        def dfs(x,y) -> None:
+        def dfs(r,c) -> None:
             for dx,dy in directions:
-                curr_r,curr_c = dx+x, dy + y
-                if valid_cell(curr_r,curr_c) and grid[curr_r][curr_c] != "#":
-                    grid[curr_r][curr_c] = "#"
+                curr_r, curr_c = dx + r, dy + c
+                if valid_cell(curr_r, curr_c) and (curr_r,curr_c) not in seen:
+                    seen.add((curr_r,curr_c))
                     dfs(curr_r,curr_c)
+
 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1" and grid[r][c] != "#":
+                if grid[r][c] == "1" and (r,c) not in seen:
                     ans +=1
-                    grid[r][c] == "#"
+                    seen.add((r,c))
                     dfs(r,c)
 
+
         return ans
+
         
