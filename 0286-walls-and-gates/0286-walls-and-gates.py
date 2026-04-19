86e9c5a53b3dae02d5b36cb2d0b14ea862478769
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-19 15:22:51 -0700

    Time: 48 ms (36.41%), Space: 26.1 MB (19.82%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -1,42 +1,30 @@
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        """
-        Do not return anything, modify rooms in-place instead.
-        """
-        ROWS = len(rooms)
-        COLS = len(rooms[0])
-        seen = set()
-        dist = 0
-        queue = deque([])
+        ROWS, COLS = len(rooms),len(rooms[0])
+        visit = set()
+        q = deque()
 
+        #first let's add all our gates to the queue and visit set
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
-                    queue.append((r,c))
-                    seen.add((r,c))
+                    q.append([r,c])
+                    visit.add((r,c))
 
         def addRoom(r,c):
-            if 0 > r or r == ROWS or 0 > c or c == COLS or (r,c) in seen or rooms[r][c] == -1: 
+            if(r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or rooms[r][c] == -1):
                 return
-            queue.append((r,c))
-            seen.add((r,c))
-
-
-        while queue:
-            for i in range(len(queue)):
-                r,c = queue.popleft()
+            visit.add((r,c))
+            q.append([r,c])
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                #labelling the min dist for that room
                 rooms[r][c] = dist
-                addRoom(r + 1, c)
-                addRoom(r - 1, c)
-                addRoom(r, c + 1)
-                addRoom(r, c - 1)
-
-            dist +=1
-
-
-                
-
-
-
-
-        
+                #adding all the four adjacent rooms to the queue
+                addRoom(r + 1,c)
+                addRoom(r - 1,c)
+                addRoom(r,c + 1)
+                addRoom(r,c - 1)
+            dist += 1
