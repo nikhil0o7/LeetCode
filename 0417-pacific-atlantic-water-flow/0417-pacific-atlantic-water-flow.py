8c749cb214f819d2e353b28d83caf1893aad12b9
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-19 17:17:40 -0700

    Time: 47 ms (18.47%), Space: 20.7 MB (78.58%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,40 +1,42 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS , COLS = len(heights), len(heights[0])
+        ROWS = len(heights)
+        COLS = len(heights[0])
         pac = [[False] * COLS for _ in range(ROWS)]
         atl = [[False] * COLS for _ in range(ROWS)]
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        # print(pac,atl)
         pacific = []
         atlantic = []
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
 
         def bfs(source,ocean):
-            q = deque(source)
-            while q:
-                r,c = q.popleft()
+            queue = deque(source)
+            while queue:
+                r,c = queue.popleft()
                 ocean[r][c] = True
                 for dx,dy in directions:
-                    row,col = r + dx, c + dy
-
+                    row = r + dx
+                    col = c + dy
                     if (0 <= row < ROWS and 0 <= col < COLS and not ocean[row][col] and heights[row][col] >= heights[r][c]):
-                        q.append((row,col))
+                        queue.append((row,col))
 
-        for c in range(COLS):
-            pacific.append((0,c))
-            atlantic.append((ROWS-1,c))
-        
+
+
         for r in range(ROWS):
             pacific.append((r,0))
             atlantic.append((r,COLS - 1))
+        
+        for c in range(COLS):
+            pacific.append((0,c))
+            atlantic.append((ROWS - 1, c))
 
-        bfs(pacific, pac)
-        bfs(atlantic, atl)
-
+        bfs(pacific,pac)
+        bfs(atlantic,atl)
         res = []
-
         for r in range(ROWS):
             for c in range(COLS):
                 if pac[r][c] and atl[r][c]:
                     res.append([r,c])
 
+
         return res
-        
