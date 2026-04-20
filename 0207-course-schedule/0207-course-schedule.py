71215a63b407e2430ffcbf77cb49a17845d7c1d4
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-19 18:25:55 -0700

    Time: 0 ms (100%), Space: 20.3 MB (94.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -6,24 +6,21 @@ class Solution:
         for src,dst in prerequisites:
             adj[src].append(dst)
             indegree[dst] += 1
-
+        
         queue = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
 
-
         while queue:
-            curr = queue.popleft()
+            i = queue.popleft()
             finish += 1
-            for d in adj[curr]:
+            for d in adj[i]:
                 indegree[d] -= 1
                 if indegree[d] == 0:
                     queue.append(d)
 
-
         return finish == numCourses
-
 
 
         
