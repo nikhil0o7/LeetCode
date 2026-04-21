0f5ce11a77ccf2cf592335a38a210f34b4143166
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-21 06:59:52 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,35 +1,27 @@
 class DSU:
-    def __init__(self, n):
+    def __init__(self,n):
         self.Parent = list(range(n))
         self.Size = [1] * n
 
-    def find(self, node):
+    def find(self,node):
         if self.Parent[node] != node:
             self.Parent[node] = self.find(self.Parent[node])
         return self.Parent[node]
 
-    def union(self, u, v) -> bool:
-        pu = self.find(u)
-        pv = self.find(v)
-
+    def union(self,u,v) -> bool:
+        pu = self.Parent[u]
+        pv = self.Parent[v]
         if pu == pv:
             return False
-
-        if self.Size[pu] < self.Size[pv]:
-            pu, pv = pv, pu
-
+        if self.Size[pv] > self.Size[pu]:
+            pu,pv = pv,pu
         self.Parent[pv] = pu
         self.Size[pu] += self.Size[pv]
         return True
 
-
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        N = len(edges)
-        dsu = DSU(N)
+        dsu = DSU(len(edges))
         for n1,n2 in edges:
-            if not dsu.union(n1 - 1, n2 - 1):
-                return [n1,n2]
-
-
-        
+            if not dsu.union(n1 - 1,n2 - 1):
+                return [n1,n2]        
