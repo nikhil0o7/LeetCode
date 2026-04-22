2d8004106da9f3e47ccc73f72b60ff351140213d
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-22 06:41:08 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #kadane's algorithm
-        # if we have get a -ve sum by adding the current element we don't take that one
+        ans = 0
         temp = 0
-        maxi = float("-inf")
-
-        for i in nums:
-            temp += i
-            maxi = max(maxi,temp)
+        for num in nums:
+            temp += num
+            ans = max(ans,temp)
             temp = 0 if temp < 0 else temp
-
-        return maxi
+        return ans
+        
