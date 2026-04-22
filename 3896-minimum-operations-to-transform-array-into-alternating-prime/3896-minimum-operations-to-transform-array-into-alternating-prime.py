2a44fdfe697155d07ff8aaf9662874c6842ad174
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-21 20:47:53 -0700

    Time: 4318 ms (36.48%), Space: 33.5 MB (84.14%) - LeetHub

---
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minOperations(self, nums: list[int]) -> int:
+        def is_prime(n: int) -> bool:
+            if n < 2:
+                return False
+
+            for i in range(2, int(n ** 0.5) + 1):
+                if n % i == 0:
+                    return False
+            return True
+        even_indices = nums[::2]
+        odd_indices = nums[1::2]
+        operations = 0
+        for k,v in enumerate(even_indices):
+            if not is_prime(v):
+                v = v + 1
+                operations += 1
+                while not is_prime(v):
+                    v = v + 1
+                    operations += 1
+
+
+        for k,v in enumerate(odd_indices):
+            if is_prime(v):
+                v = v + 1
+                operations += 1
+                while is_prime(v):
+                    v = v + 1
+                    operations += 1
+        return operations
+        
