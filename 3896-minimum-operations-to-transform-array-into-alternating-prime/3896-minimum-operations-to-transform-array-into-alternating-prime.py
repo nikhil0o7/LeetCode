9d87f3661e33668c78b1dc4472270ec396780f34
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-21 20:48:54 -0700

    Time: 4344 ms (35.08%), Space: 33.5 MB (71.71%) - LeetHub

--- 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.py
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.py
@@ -8,24 +8,24 @@ class Solution:
                 if n % i == 0:
                     return False
             return True
+            
         even_indices = nums[::2]
         odd_indices = nums[1::2]
         operations = 0
-        for k,v in enumerate(even_indices):
-            if not is_prime(v):
-                v = v + 1
-                operations += 1
-                while not is_prime(v):
+        for k,v in enumerate(nums):
+            if k % 2 == 0 :
+                if not is_prime(v):
                     v = v + 1
                     operations += 1
-
-
-        for k,v in enumerate(odd_indices):
-            if is_prime(v):
-                v = v + 1
-                operations += 1
-                while is_prime(v):
+                    while not is_prime(v):
+                        v = v + 1
+                        operations += 1
+            else:
+                if is_prime(v):
                     v = v + 1
                     operations += 1
+                    while is_prime(v):
+                        v = v + 1
+                        operations += 1
         return operations
         
