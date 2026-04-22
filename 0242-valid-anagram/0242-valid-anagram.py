ea9475836a477f938d605e49c384f0db12ca5d12
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-21 19:44:00 -0700

    Time: 7 ms (90.81%), Space: 19.7 MB (24.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,4 +2,6 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         countS = Counter(s)
         countT = Counter(t)
+        print(countS, countT)
         return countS == countT
+        
