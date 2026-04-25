0989f6172854a2a503c063689e3ce5daee2903c3
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-25 11:48:00 -0700

    Time: 66 ms (87.35%), Space: 19.8 MB (12.35%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,31 +1,26 @@
 class Codec:
-    def encode(self, strs: List[str]) -> str:
-        """Encodes a list of strings to a single string.
-        """
-        encoded_str = ''
+    def encode(self, strs):
+        # Initialize an empty string to hold the encoded string.
+        encoded_string = ''
         for s in strs:
-            curr_s =  str(len(s)) +  ':/' + s
-            print(curr_s)
-            encoded_str += curr_s
-        return encoded_str
-        
+            # Append the length, the delimiter, and the string itself.
+            encoded_string += str(len(s)) + '/:' + s
+        print(encoded_string)
+        return encoded_string
 
-    def decode(self, s: str) -> List[str]:
-        """Decodes a single string to a list of strings.
-        """
-        decoded_str = []
+    def decode(self, s):
+        # Initialize a list to hold the decoded strings.
+        decoded_strings = []
         i = 0
         while i < len(s):
-            delim = s.find(':/',i)
+            # Find the delimiter.
+            delim = s.find('/:', i)
+            # Get the length, which is before the delimiter.
             length = int(s[i:delim])
-            curr_s = s[delim+2 : delim+2+length]
-            decoded_str.append(curr_s)
-            i = length +delim + 2
-
-        return decoded_str
-        
-
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(strs))
+            # Get the string, which is of 'length' length after the delimiter.
+            str_ = s[delim+2 : delim+2+length]
+            # Add the string to the list.
+            decoded_strings.append(str_)
+            # Move the index to the start of the next length.
+            i = delim + 2 + length
+        return decoded_strings
