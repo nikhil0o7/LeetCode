15b4da93b8b9381431811e8f6d0a943de8cceeac
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-26 15:06:30 -0700

    Time: 39 ms (54.3%), Space: 34.2 MB (53.18%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,4 +1,4 @@
-class TrieNode:
+class TrieNode():
     def __init__(self):
         self.children = {}
         self.endOfWord = False
@@ -10,34 +10,36 @@ class Trie:
         
 
     def insert(self, word: str) -> None:
-        cur = self.root
-
+        curr = self.root
         for c in word:
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
-            cur = cur.children[c]
-        cur.endOfWord = True
-    
-    def search(self, word: str) -> bool:
-        cur = self.root
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
 
-        for c in word:
-            if c not in cur.children:
-                return False
-            cur = cur.children[c]
+            curr = curr.children[c]
 
-        return cur.endOfWord
+        curr.endOfWord = True
         
-    def startsWith(self, prefix: str) -> bool:
-        cur = self.root
 
-        for c in prefix:
-            if c not in cur.children:
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
                 return False
-            cur = cur.children[c]
-            
+            curr = curr.children[c]
+
+        return curr.endOfWord
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+
         return True
         
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
