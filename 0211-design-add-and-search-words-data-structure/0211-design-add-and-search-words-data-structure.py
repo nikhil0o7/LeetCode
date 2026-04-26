1668338d804d7b20c22c8ab67be1db7d8c73761c
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-26 15:13:11 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,4 +1,4 @@
-class TrieNode:
+class TrieNode():
     def __init__(self):
         self.children = {}
         self.endOfWord = False
@@ -7,26 +7,28 @@ class WordDictionary:
 
     def __init__(self):
         self.root = TrieNode()
+        
 
     def addWord(self, word: str) -> None:
         curr = self.root
-
         for c in word:
             if c not in curr.children:
                 curr.children[c] = TrieNode()
+
             curr = curr.children[c]
         curr.endOfWord = True
         
+
     def search(self, word: str) -> bool:
-        def dfs(j, root):
+        def dfs(i,root):
             curr = root
-            for i in range(j, len(word)):
-                c = word[i]
+            for j in range(i, len(word)):
+                c = word[j]
                 if c == ".":
                     for child in curr.children.values():
-                        if dfs(i + 1, child):
+                        if dfs(j + 1, child):
                             return True
-                    return False
+                        return False
                 else:
                     if c not in curr.children:
                         return False
