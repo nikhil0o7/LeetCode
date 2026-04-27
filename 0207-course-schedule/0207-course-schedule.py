027acdcf2cfd02230b7fb739cd09dc3bc55bb998
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-26 18:09:45 -0700

    Time: 0 ms (100%), Space: 20.4 MB (80.73%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
         adj = [[] for _ in range(numCourses)]
-        finish = 0
+        indegree = [0] * numCourses
         for src,dst in prerequisites:
             adj[src].append(dst)
             indegree[dst] += 1
-        
+
         queue = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
 
+        finish = 0
         while queue:
-            i = queue.popleft()
+            curr = queue.popleft()
             finish += 1
-            for d in adj[i]:
-                indegree[d] -= 1
-                if indegree[d] == 0:
-                    queue.append(d)
+            for i in adj[curr]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    queue.append(i)
 
         return finish == numCourses
-
-
         
