9e3b78d6e2d07a649445ab93435e3b062e135367
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-27 18:26:28 -0700

    Time: 40 ms (6.66%), Space: 19.6 MB (84.57%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,31 +1,32 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ans = 0
-        seen = set()
         ROWS = len(grid)
         COLS = len(grid[0])
+        area = 0
         directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        seen = set()
 
-        def valid_cell(r,c) -> bool:
+        def valid_cell(r,c):
             return 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == 1
-
         for r in range(ROWS):
             for c in range(COLS):
-                if (r,c) not in seen and grid[r][c] == 1:
+                if grid[r][c]==1:
+                    stack = []
+                    stack.append((r,c))
                     seen.add((r,c))
                     shape = 0
-                    stack = [(r,c)]
                     while stack:
-                        row,col = stack.pop()
+                        r,c = stack.pop()
                         shape +=1
                         for dx,dy in directions:
-                            curr_r, curr_c = row + dx, col + dy
+                            curr_r = r + dx
+                            curr_c = c + dy
                             if valid_cell(curr_r,curr_c) and (curr_r,curr_c) not in seen:
                                 seen.add((curr_r,curr_c))
                                 stack.append((curr_r,curr_c))
-                    ans = max(shape,ans)
 
+                    area = max(shape,area)
 
-        return ans
+        return area
 
         
