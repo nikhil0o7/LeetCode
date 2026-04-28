79927607d3cd1f904c7fdcd6cb6c78889c034c50
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-27 17:44:45 -0700

    Time: 4 ms (68.04%), Space: 20.2 MB (92.33%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,31 +1,32 @@
 class DSU:
-    def __init__(self,n):
-        self.Parent = list((range(n)))
+    def __init__(self, n):
+        self.Parent = list(range(n))
         self.Size = [1] * n
 
-    def find(self,node) -> int:
+    def find(self,node):
         if self.Parent[node] != node:
             self.Parent[node] = self.find(self.Parent[node])
         return self.Parent[node]
-
-    def union(self, u, v) -> int:
+    
+    def union(self, u, v):
         pu = self.find(u)
         pv = self.find(v)
         if pu == pv:
-            return 0
-        if self.Size[pu] < self.Size[pv]:
+            return False
+        if self.Size[pv] > self.Size[pu]:
             pu,pv = pv,pu
         self.Parent[pv] = pu
         self.Size[pu] += self.Size[pv]
-        return 1
-
+        return True
 
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        res = n
         dsu = DSU(n)
+        comps = n
         for n1,n2 in edges:
-            res -= dsu.union(n1,n2)
+            if dsu.union(n1,n2):
+                comps -= 1
 
-        return res
+        return comps
+
         
