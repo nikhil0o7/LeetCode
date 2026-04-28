24db61be630f3d7d63a4e2d0a8dd7245d2594b80
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-27 18:51:36 -0700

    Time: 53 ms (29.91%), Space: 26.4 MB (16.1%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -1,30 +1,32 @@
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        ROWS, COLS = len(rooms),len(rooms[0])
-        visit = set()
-        q = deque()
-
-        #first let's add all our gates to the queue and visit set
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        ROWS = len(rooms)
+        COLS = len(rooms[0])
+        queue = deque()
+        seen = set()
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
-                    q.append([r,c])
-                    visit.add((r,c))
-
-        def addRoom(r,c):
-            if(r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or rooms[r][c] == -1):
-                return
-            visit.add((r,c))
-            q.append([r,c])
+                    queue.append((r,c))
+                    seen.add((r,c))
         dist = 0
-        while q:
-            for i in range(len(q)):
-                r,c = q.popleft()
-                #labelling the min dist for that room
+
+        def addRoom(curr_r,curr_c):
+            if 0 <= curr_r < ROWS and 0 <= curr_c < COLS and (curr_r,curr_c) not in seen and rooms[curr_r][curr_c] != -1:
+                seen.add((curr_r,curr_c))
+                queue.append([curr_r,curr_c])
+
+        while queue:
+            for i in range(len(queue)):
+                r,c = queue.popleft()
                 rooms[r][c] = dist
-                #adding all the four adjacent rooms to the queue
                 addRoom(r + 1,c)
                 addRoom(r - 1,c)
-                addRoom(r,c + 1)
-                addRoom(r,c - 1)
+                addRoom(r, c + 1)
+                addRoom(r, c - 1)
+
             dist += 1
