cdb8549f430071828bd148b04c9a6b8bbd389d7c
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-27 17:52:44 -0700

    Time: 3 ms (59.94%), Space: 19.6 MB (80.42%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -2,26 +2,31 @@ class DSU:
     def __init__(self,n):
         self.Parent = list(range(n))
         self.Size = [1] * n
-
+    
     def find(self,node):
         if self.Parent[node] != node:
             self.Parent[node] = self.find(self.Parent[node])
         return self.Parent[node]
 
-    def union(self,u,v) -> bool:
+    def union(self, u, v):
         pu = self.find(u)
         pv = self.find(v)
         if pu == pv:
-            return False
-        if self.Size[pv] > self.Size[pu]:
-            pu,pv = pv,pu
+            return True
+        if self.Size[pu] < self.Size[pv]:
+            pv,pu = pu,pv
         self.Parent[pv] = pu
-        self.Size[pu] += self.Size[pv]
-        return True
+        self.Size[pu] += pv
+        return False
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        dsu = DSU(len(edges))
+        ans = []
+        N = len(edges) + 1
+        dsu = DSU(N)
         for n1,n2 in edges:
-            if not dsu.union(n1 - 1,n2 - 1):
-                return [n1,n2]        
+            if dsu.union(n1,n2):
+                ans.append([n1,n2])
+
+        return ans[-1]
+        
