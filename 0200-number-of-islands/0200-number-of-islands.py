32848aa114d3baa3a22cf7fd1a6d2c9b60ce7c41
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-27 18:38:19 -0700

    Time: 264 ms (24.63%), Space: 26.7 MB (24.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,28 +2,25 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
+        seen = set()
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
         ans = 0
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
         def valid_cell(r,c) -> bool:
             return 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == "1"
 
-        def dfs(r,c) -> None:
-            for dx,dy in directions:
-                curr_r, curr_c = dx + r, dy + c
-                if valid_cell(curr_r, curr_c) and grid[curr_r][curr_c] != "#":
-                    grid[curr_r][curr_c] = "#"
-                    dfs(curr_r,curr_c)
-
-
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1" and grid[r][c] != "#":
-                    ans +=1
-                    grid[r][c] = "#"
-                    dfs(r,c)
-
-
-        return ans
-
-        
+                if grid[r][c] == "1" and (r,c) not in seen:
+                    stack = []
+                    stack.append((r,c))
+                    seen.add((r,c))
+                    ans+=1
+                    while stack:
+                        row,col = stack.pop()
+                        for dx,dy in directions:
+                            curr_r,curr_c= dx + row, dy + col
+                            if valid_cell(curr_r,curr_c) and (curr_r,curr_c) not in seen:
+                                seen.add((curr_r,curr_c))
+                                stack.append((curr_r,curr_c))
+        return ans        
