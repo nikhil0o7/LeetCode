1e6c8a642d2f88a9e013e6840c4883b6d8fa569c
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-28 20:12:00 -0700

    Time: 207 ms (25.06%), Space: 27.2 MB (17.67%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,11 +3,9 @@ class Solution:
         n = len(s)
         dp = [[False] * n for _ in range(n)]
         ans = 0
-
         for i in range(n):
             dp[i][i] = True
-            ans +=1
-
+            ans += 1
         for i in range(n - 1):
             if s[i] == s[i + 1]:
                 dp[i][i + 1] = True
@@ -17,6 +15,7 @@ class Solution:
             for i in range(n - diff):
                 j = i + diff
                 if s[i] == s[j] and dp[i + 1][j - 1]:
-                    dp[i][j] = True
                     ans += 1
+                    dp[i][j] = True
+
         return ans
