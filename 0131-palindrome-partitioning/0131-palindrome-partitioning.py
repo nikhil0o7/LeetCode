7f4fb56a42e2063a17961a27859a495c8d310abb
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-04-28 17:58:22 -0700

    Time: 43 ms (62.8%), Space: 34.3 MB (20.38%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,13 +1,12 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        
-        def backtrack(i):
+        def backtrack(i) -> None:
             if i >= len(s):
                 res.append(part.copy())
                 return
 
             for j in range(i, len(s)):
-                if self.isPalindrome(s,i,j):
+                if self.isPalindrome(s, i, j):
                     part.append(s[i: j + 1])
                     backtrack(j + 1)
                     part.pop()
@@ -17,13 +16,13 @@ class Solution:
         backtrack(0)
         return res
 
-    def isPalindrome(self,s,l ,r):
-        while l <= r:
-            if s[l] != s[r]:
+    def isPalindrome(self, s, i, j) -> bool:
+        l,r = i, j
+        while i < j:
+            if s[i] != s[j]:
                 return False
-
-            l += 1
-            r -= 1
-
+            i += 1
+            j -= 1
         return True
+    
         
