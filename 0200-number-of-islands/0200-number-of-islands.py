d39b72d3682e2504a2933c62cc890c9d3e493a71
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-01 20:40:40 -0700

    Time: 266 ms (22.55%), Space: 26.8 MB (21.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
         ROWS = len(grid)
         COLS = len(grid[0])
         seen = set()
-        directions = [(0,1),(1,0),(0,-1),(0,1)]
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
         ans = 0
 
-        def valid_cell(r,c) -> bool:
-            return 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == "1" and (r,c) not in seen
+        def valid_cell(r, c) -> bool:
+            return 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == "1"
 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1" and (r,c) not in seen:
-                    seen.add((r,c))
-                    stack = []
+                if grid[r][c] == "1" and (r, c) not in seen:
                     ans += 1
-                    stack.append((r,c))
+                    seen.add((r, c))
+                    stack = [(r, c)]
+
                     while stack:
-                        r,c = stack.pop()
-                        for dx,dy in directions:
-                            curr_r,curr_c = r + dx, c + dy
-                            if valid_cell(curr_r,curr_c):
-                                seen.add((curr_r,curr_c))
-                                stack.append((curr_r,curr_c))
+                        row, col = stack.pop()
+
+                        for dx, dy in directions:
+                            curr_r = row + dx
+                            curr_c = col + dy
+
+                            if valid_cell(curr_r, curr_c) and (curr_r, curr_c) not in seen:
+                                seen.add((curr_r, curr_c))
+                                stack.append((curr_r, curr_c))
 
         return ans
-
-
-        
