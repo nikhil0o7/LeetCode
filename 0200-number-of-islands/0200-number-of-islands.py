3a91fed872a76cf4afc96c459139b8eaae8be770
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-01 20:33:38 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,24 +3,28 @@ class Solution:
         ROWS = len(grid)
         COLS = len(grid[0])
         seen = set()
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        directions = [(0,1),(1,0),(0,-1),(0,1)]
         ans = 0
 
         def valid_cell(r,c) -> bool:
-            return 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == "1"
+            return 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == "1" and (r,c) not in seen
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1" and (r,c) not in seen:
-                    stack = []
-                    stack.append((r,c))
                     seen.add((r,c))
-                    ans+=1
+                    stack = []
+                    ans += 1
+                    stack.append((r,c))
                     while stack:
-                        row,col = stack.pop()
+                        r,c = stack.pop()
                         for dx,dy in directions:
-                            curr_r,curr_c= dx + row, dy + col
-                            if valid_cell(curr_r,curr_c) and (curr_r,curr_c) not in seen:
+                            curr_r,curr_c = r + dx, c + dy
+                            if valid_cell(curr_r,curr_c):
                                 seen.add((curr_r,curr_c))
                                 stack.append((curr_r,curr_c))
-        return ans        
+
+        return ans
+
+
+        
