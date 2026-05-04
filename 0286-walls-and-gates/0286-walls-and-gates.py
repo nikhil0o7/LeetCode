4c966378fe5b546e922f0a325da47d224a8b334f
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-04 06:47:30 -0700

    Time: 51 ms (33.23%), Space: 26 MB (21.36%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -6,27 +6,33 @@ class Solution:
         ROWS = len(rooms)
         COLS = len(rooms[0])
         queue = deque()
+        dist = 0
         seen = set()
-        
+
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
                     queue.append((r,c))
                     seen.add((r,c))
-        dist = 0
 
-        def addRoom(curr_r,curr_c):
-            if 0 <= curr_r < ROWS and 0 <= curr_c < COLS and (curr_r,curr_c) not in seen and rooms[curr_r][curr_c] != -1:
-                seen.add((curr_r,curr_c))
-                queue.append([curr_r,curr_c])
+        def addRoom(r,c) -> None:
+            if 0 <= r < ROWS and 0 <= c < COLS and (r,c) not in seen and rooms[r][c] != -1:
+                queue.append((r,c))
+                seen.add((r,c))
 
         while queue:
             for i in range(len(queue)):
                 r,c = queue.popleft()
                 rooms[r][c] = dist
-                addRoom(r + 1,c)
-                addRoom(r - 1,c)
+
+                addRoom(r + 1, c)
+                addRoom(r - 1, c)
                 addRoom(r, c + 1)
                 addRoom(r, c - 1)
 
-            dist += 1
+            dist+=1
+
+        
+
+
+        
