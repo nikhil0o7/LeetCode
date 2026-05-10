435754ab879ebb04d1ca1f5fd5e389ce56a60c1a
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-10 12:58:56 -0700

    Time: 923 ms (18.32%), Space: 70.8 MB (12.3%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,17 +4,15 @@ class Solution:
         def dfs(amount):
             if amount == 0:
                 return 0
-            
             if amount in memo:
                 return memo[amount]
 
             res = float("inf")
             for coin in coins:
                 if amount - coin >= 0:
-                    res = min(res, 1 + dfs(amount - coin))
+                    res = min(res,  1 + dfs(amount - coin))
+
             memo[amount] = res
             return res
-
         minCoins = dfs(amount)
         return -1 if minCoins == float("inf") else minCoins
-        
