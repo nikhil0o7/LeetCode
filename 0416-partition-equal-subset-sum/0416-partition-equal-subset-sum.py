17d5ae6f9dcce21496d56cab6e09ef9ad7d4cbcf
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-11 06:38:06 -0700

    Time: 259 ms (89.15%), Space: 20.5 MB (56.3%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,20 +4,17 @@ class Solution:
             return False
 
         dp = set()
-        dp.add(0)
         target = sum(nums) // 2
-        print(target)
-        for i in range(len(nums) - 1, -1, -1):
+        dp.add(0)
+        for i in range(len(nums)):
             nextDP = set()
-            
+
             for t in dp:
-                if (t + nums[i]) == target:
-                    print(dp)
+                if t + nums[i] == target:
                     return True
                 nextDP.add(t + nums[i])
                 nextDP.add(t)
             dp = nextDP
 
-        print(target)
-        print(dp)
-        return True if target in dp else False
+        return False
+        
