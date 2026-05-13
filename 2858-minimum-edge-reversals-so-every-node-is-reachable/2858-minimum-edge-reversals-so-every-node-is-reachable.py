fee1ac67916585ffe6da0fb74d369cb6ca200ad0
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-13 06:40:33 -0700

    Time: 399 ms (50.47%), Space: 179.8 MB (31.79%) - LeetHub

--- 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.py
+++ 2858-minimum-edge-reversals-so-every-node-is-reachable/2858-minimum-edge-reversals-so-every-node-is-reachable.py
@@ -1,22 +1,41 @@
 class Solution:
     def minEdgeReversals(self, n: int, edges: List[List[int]]) -> List[int]:
-        g,ans,zeroCnt = defaultdict(list), defaultdict(int),0
-        for u,v in edges:
-            g[u].append((v,1))
-            g[v].append((u,-1))
+        graph = defaultdict(list)
 
-        queue = deque([(0,0)])
+        for u, v in edges:
+            graph[u].append((v, 0))  # correct direction from u to v
+            graph[v].append((u, 1))  # would need reversal
 
-        while queue:
-            par,parCnt = queue.pop()
-            ans[par] = parCnt
+        res = [0] * n
 
-            for chd,cnt in g[par]:
-                if chd in ans: continue
-                zeroCnt+= cnt
-                queue.append((chd, cnt + parCnt))
+        def dfs_count(node, parent):
+            reversals = 0
 
-        
-        ans0 = (n - 1 - zeroCnt) // 2
-        return [ans[i] + ans0 for i in range(n)] 
-        
+            for nei, cost in graph[node]:
+                if nei == parent:
+                    continue
+                reversals += cost + dfs_count(nei, node)
+
+            return reversals
+
+        res[0] = dfs_count(0, -1)
+
+        def reroot(node, parent):
+            for nei, cost in graph[node]:
+                if nei == parent:
+                    continue
+
+                if cost == 0:
+                    # Edge node -> nei was good for node as root.
+                    # If nei becomes root, this edge becomes bad.
+                    res[nei] = res[node] + 1
+                else:
+                    # Edge nei -> node was bad for node as root.
+                    # If nei becomes root, this edge becomes good.
+                    res[nei] = res[node] - 1
+
+                reroot(nei, node)
+
+        reroot(0, -1)
+
+        return res
