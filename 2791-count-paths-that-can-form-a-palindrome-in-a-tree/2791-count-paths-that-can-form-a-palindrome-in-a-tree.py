e3f12cf96506152bac789b1eb83a376572702285
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-16 14:47:17 -0700

    Time: 1818 ms (15.65%), Space: 201.7 MB (14.51%) - LeetHub

--- 2791-count-paths-that-can-form-a-palindrome-in-a-tree/2791-count-paths-that-can-form-a-palindrome-in-a-tree.py
+++ 2791-count-paths-that-can-form-a-palindrome-in-a-tree/2791-count-paths-that-can-form-a-palindrome-in-a-tree.py
@@ -21,17 +21,18 @@ class Solution:
             for child in graph[node]:
                 char_index = ord(s[child]) - ord('a')
                 child_mask = mask ^ (1 << char_index)
-
+                # print(child_mask)
+                # print(count)/
                 # Case 1: same mask
                 ans += count[child_mask]
-
+                
                 # Case 2: differs by exactly one character
                 for i in range(26):
                     ans += count[child_mask ^ (1 << i)]
 
                 # Store this node's root-to-node mask
                 count[child_mask] += 1
-
+                print(child,child_mask)
                 dfs(child, child_mask)
 
         dfs(0, 0)
