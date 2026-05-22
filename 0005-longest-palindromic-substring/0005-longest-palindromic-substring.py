97772e725a41663b69a678e38093e4ca4303209e
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-22 06:54:55 -0700

    Time: 1468 ms (25.96%), Space: 27 MB (10.14%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,26 +3,25 @@ class Solution:
         if len(s) == 1:
             return s
         n = len(s)
-        dp = [[False] * n for _ in range(len(s))]
-        ans = [0,0]
-        for i in range(n):
+        dp = [[False] * len(s) for _ in range(n)]
+        for i in range(len(s)):
             dp[i][i] = True
-
+        ans = [0,0]
         for i in range(n - 1):
             if s[i] == s[i + 1]:
                 dp[i][i + 1] = True
                 ans = [i, i + 1]
 
-        
-        for diff in range(2, n):
+        for diff in range(2,n):
             for i in range(n - diff):
                 j = i + diff
                 if s[i] == s[j] and dp[i + 1][j - 1]:
                     dp[i][j] = True
-                    ans = [i, j]
+                    ans = [i,j]
 
 
         x,y = ans
         return s[x : y + 1]
+        
 
-
+        
