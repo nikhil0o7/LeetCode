d5fd6e97b14cbd44243b149ee6b3c559b62b6e33
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-21 19:37:19 -0700

    Time: 179 ms (84.94%), Space: 30.1 MB (88.04%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         max_deque = deque()
@@ -9,20 +10,25 @@ class Solution:
 
             while max_deque and max_deque[-1] < nums[right]:
                 max_deque.pop()
+
             max_deque.append(nums[right])
 
             while min_deque and min_deque[-1] > nums[right]:
                 min_deque.pop()
             min_deque.append(nums[right])
 
+
             while max_deque[0] - min_deque[0] > limit:
                 if max_deque[0] == nums[left]:
                     max_deque.popleft()
                 if min_deque[0] == nums[left]:
                     min_deque.popleft()
+
                 left += 1
 
             max_length = max(max_length, right - left + 1)
 
+
         return max_length
+
         
