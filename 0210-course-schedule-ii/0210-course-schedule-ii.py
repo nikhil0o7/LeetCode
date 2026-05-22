0240d41101cb87e1e9cb176ca0bbf08187d9f3c2
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-22 06:39:29 -0700

    Time: 6 ms (32.16%), Space: 20.3 MB (73.8%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -4,7 +4,6 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         adj = [[] for _ in range(numCourses)]
         indegree = [0] * numCourses
-        print(adj)
         queue = deque()
         for src,dst in prerequisites:
             adj[src].append(dst)
