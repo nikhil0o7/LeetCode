5c906e990a27607a1e4bc464ec8768a2fc0ee266
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-05-22 06:30:23 -0700

    Time: 495 ms (92.28%), Space: 61.3 MB (76.85%) - LeetHub

--- 1429-first-unique-number/1429-first-unique-number.py
+++ 1429-first-unique-number/1429-first-unique-number.py
@@ -1,11 +1,14 @@
+from collections import deque
+from typing import List
+
 class FirstUnique:
 
     def __init__(self, nums: List[int]):
         self._isUnique = {}
-        self._queue = deque(nums)
+        self._queue = deque()
+
         for num in nums:
             self.add(num)
-        
 
     def showFirstUnique(self) -> int:
         while self._queue and not self._isUnique[self._queue[0]]:
@@ -15,18 +18,10 @@ class FirstUnique:
             return self._queue[0]
 
         return -1
-        
 
     def add(self, value: int) -> None:
         if value not in self._isUnique:
             self._queue.append(value)
             self._isUnique[value] = True
         else:
-            self._isUnique[value] = False 
-        
-
-
-# Your FirstUnique object will be instantiated and called as such:
-# obj = FirstUnique(nums)
-# param_1 = obj.showFirstUnique()
-# obj.add(value)
+            self._isUnique[value] = False
