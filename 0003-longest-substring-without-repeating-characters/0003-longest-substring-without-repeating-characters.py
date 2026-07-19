df8589e9707abe83fba311dde56f7f92587ae85b
Nikhil Srirangam <37364736+nikhil0o7@users.noreply.github.com>
2026-07-19 11:24:40 -0700

    Time: 19 ms (22.26%), Space: 19.2 MB (92.68%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,16 +3,18 @@ class Solution:
         countS = defaultdict(int)
         l = r = 0
         ans = 0
+
         while r < len(s):
             countS[s[r]] += 1
-            # print(countS)
             while countS[s[r]] > 1:
                 left = s[l]
                 countS[left] -= 1
-                l+= 1
+                l += 1
             ans = max(ans, r - l + 1)
-            r+=1
+            r += 1
 
-        return ans
+        return ans 
+            
+
 
         
